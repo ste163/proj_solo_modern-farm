e3c628c3bcc61443517c9f2c151c79a01a255b0d
ste163 <samtedwards@pm.me>
2020-08-20 17:10:49 -0400

    Added addPlant and usePlants function in field.js

--- src/scripts/field.js
+++ src/scripts/field.js
@@ -1,10 +1,26 @@
-const field = [];
+const arrField = [];
 
 export const addPlant = (objSeed) => {
-    field.push(objSeed);
+    //Check to see if objSeed is an object or an array
+    //If it's an array, we might need to slice it.
+    //To break the objects from the array.
+    if (Array.isArray(objSeed) === false) {
+        console.log("Not an array, so it is an object! Pushing to field.");
+        arrField.push(objSeed);
+    } else {
+        console.log("It's an array!")
+        const seeds = objSeed.slice();
+        // Loops through the objSeed copy
+        // it takes each object, saves that object to moveCorn,
+        // then pushes moveCorn to the main array.
+        for (const seed in seeds) {
+              const moveCorn = seeds[seed];
+              arrField.push(moveCorn);
+        }
+    }  
 }
 
 export const usePlants = () => {
-    
-
-}
+    let arrFieldCopy = arrField;
+    return arrFieldCopy
+};
--- src/scripts/main.js
+++ src/scripts/main.js
@@ -7,6 +7,8 @@ import { createSoybean } from "./seeds/soybean.js";
 import { createSunflower } from "./seeds/sunflower.js";
 import { createWheat } from "./seeds/wheat.js";
 import { createCorn } from "./seeds/corn.js";
+import { addPlant } from "./field.js";
+import { usePlants } from "./field.js";
 
 //Stores the objs into varaibles so we can use them
 const yearlyPlan = createPlan();
@@ -16,11 +18,17 @@ const soybeanSeed = createSoybean();
 const sunflowerSeed = createSunflower();
 const wheatSeed = createWheat();
 const cornSeed = createCorn();
+// const addedPlant = addPlant();
+const usedPlants = usePlants();
 
-console.log(yearlyPlan);
-console.log(asparagusSeed);
-console.log(potatoSeed);
-console.log(soybeanSeed);
-console.log(sunflowerSeed);
-console.log(wheatSeed);
-console.log(cornSeed);
+// console.log(yearlyPlan);
+// console.log(asparagusSeed);
+// console.log(potatoSeed);
+// console.log(soybeanSeed);
+// console.log(sunflowerSeed);
+// console.log(wheatSeed);
+// console.log(cornSeed);
+
+//Tests to see if field.js is working
+console.log("ADD PLANT FUNCT", addPlant(createWheat()));
+console.log("USE PLANTS FUNCT", usePlants());
