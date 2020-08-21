eadc924c21890708f4474c8d67d6cf5ad37093e4
ste163 <samtedwards@pm.me>
2020-08-21 15:58:18 -0400

    Completed second task

--- src/scripts/harvester.js
+++ src/scripts/harvester.js
@@ -1,13 +1,16 @@
-import { usePlants } from "./field.js";
 
-const plantedSeeds = usePlants();
 
 export const harvestPlants = (arrPlants) => {
-    console.log("CURRENT PLANTS TO HARVEST", plantedSeeds);
-    
+    console.log("CURRENT PLANTS TO HARVEST", arrPlants);
+    debugger;
+
+    for (const plant of arrPlants) {
+        console.log(plant.output);
+    }
+
     //Function Goal
-    //Add the parameter of our array of seeds in the field made during in the tractor.js (this will happen in main.js)
-    //Iterate through each plant, getting its output property value.
+    //DONE - Add the parameter of our array of seeds in the field
+    //DONE - Iterate through each plant, getting its output property value.
     //Based on that objects value, we add that many to a new array, called something like arrProduce
         //The EXCEPTION is corn. Only take half of that ouput, because we're "selling" half of it.
     //At the end, return the produce array
--- src/scripts/main.js
+++ src/scripts/main.js
@@ -3,9 +3,12 @@ console.log("MAIN MODULE LOADED")
 import { createPlan } from "./plan.js";
 import { plantSeeds } from "./tractor.js";
 import { harvestPlants } from "./harvester.js";
+import { usePlants } from "./field.js";
+
+const plantedSeeds = usePlants();
 
 //Stores the objs into varaibles so we can use them
 const yearlyPlan = createPlan();
 
-console.log(plantSeeds(yearlyPlan));
-harvestPlants();
+plantSeeds(yearlyPlan);
+harvestPlants(plantedSeeds);
