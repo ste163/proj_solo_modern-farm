620dd9e4bfd6b695a0bf1f5a74f01c3768bf506f
ste163 <samtedwards@pm.me>
2020-08-21 11:42:55 -0400

    added switch statement to tractor.js

--- src/scripts/tractor.js
+++ src/scripts/tractor.js
@@ -9,15 +9,38 @@ export const plantSeeds = (plan) => {
         console.log(plan); //ITERATING OVER THE FIRST ARRAY AND RETURNING THE INSIDE ARRAYS
         let seed = plan;
         for (seed of plan) {
-            console.log(seed);
+            debugger;
+            console.log(seed); //APPEARS to be working correctly...
+            //switch statement for what the name is
+            let seedInPlan = seed;
+            switch (seedInPlan) {
+                case 'Asparagus':
+                console.log("SWITCH Asparagus");
+                break;
+                case 'Corn':
+                console.log("SWITCH Corn");
+                break;
+                case 'Potato':
+                console.log("SWITCH Potato");
+                break;
+                case 'Wheat':
+                console.log("SWITCH Wheat");
+                break;
+                case 'Soybean':
+                console.log("SWITCH Soybean");
+                break;
+                case 'Sunflower':
+                console.log("SWITCH Sunflower");
+                break;
+            }
+            //If the name matches the switch, create the seed and push to array.
         }
     }
         
-        //Loop through the array of each object.
-            //Based on what the object's TYPE is, match that with its corresponding
+        //DONE - Loop through the array.
+            //Based on what the array's name is, match that with its corresponding
             //createSeed function.
             //After the seed is created, add it to the array
             //Continue until everything has been created, then go to the next array
     //Once everything has been added, return the sliced array.
-
 }
