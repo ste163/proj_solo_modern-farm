c741061654b8e974ddaae0fc1709e4bd09331283
ste163 <samtedwards@pm.me>
2020-08-21 15:34:08 -0400

    initial pseudo-code written for harvester.js

---
+++ src/scripts/harvester.js
@@ -0,0 +1,15 @@
+export const harvestPlants = (arrPlants) => {
+
+    //Function Goal
+    //Add the parameter of our array of seeds in the field made during in the tractor.js (this will happen in main.js)
+    //Iterate through each plant, getting its output property value.
+    //Based on that objects value, we add that many to a new array, called something like arrProduce
+        //The EXCEPTION is corn. Only take half of that ouput, because we're "selling" half of it.
+    //At the end, return the produce array
+
+
+
+    // function will return an array of seed objects
+
+
+}
