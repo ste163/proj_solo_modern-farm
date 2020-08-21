0af570abc5810d711f43e856bd98102b8ada3a1a
ste163 <samtedwards@pm.me>
2020-08-21 15:47:34 -0400

    Added plants in field array to harvester.js

--- src/scripts/harvester.js
+++ src/scripts/harvester.js
@@ -1,5 +1,10 @@
-export const harvestPlants = (arrPlants) => {
+import { usePlants } from "./field.js";
 
+const plantedSeeds = usePlants();
+
+export const harvestPlants = (arrPlants) => {
+    console.log("CURRENT PLANTS TO HARVEST", plantedSeeds);
+    
     //Function Goal
     //Add the parameter of our array of seeds in the field made during in the tractor.js (this will happen in main.js)
     //Iterate through each plant, getting its output property value.
@@ -10,6 +15,4 @@ export const harvestPlants = (arrPlants) => {
 
 
     // function will return an array of seed objects
-
-
 }
--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,9 +1,11 @@
-console.log("Welcome to the main module")
+console.log("MAIN MODULE LOADED")
 
 import { createPlan } from "./plan.js";
 import { plantSeeds } from "./tractor.js";
+import { harvestPlants } from "./harvester.js";
 
 //Stores the objs into varaibles so we can use them
 const yearlyPlan = createPlan();
 
 console.log(plantSeeds(yearlyPlan));
+harvestPlants();
